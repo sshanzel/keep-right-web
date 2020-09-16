309dc46a85c29dc6b240db53a5c528e64ec077ae
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-16 12:18:59 +0400

    HTTP Service

---
+++ src/services/http.service.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+let jwtHeader = '';
+
+axios.defaults.baseURL = `https://localhost/api`;
+
+axios.interceptors.response.use(
+  success => Promise.resolve(success),
+  error => {
+    const expectedError =
+      error.response && error.response.status >= 400 && error.response.status < 500;
+
+    if (!expectedError) {
+      // logger service goes here
+      console.log('Logging the error', error);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export const getHeaderAuthToken = () => jwtHeader;
+
+export function setHeaderAuthToken(token: string) {
+  jwtHeader = token;
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
+
+axios.defaults.headers['Access-Control-Allow-Origin'] = `*`;
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  patch: axios.patch,
+  put: axios.put,
+  delete: axios.delete,
+};
