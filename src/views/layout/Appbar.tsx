b02902a15cb4a4652b911504f2012527f85d5368
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-18 16:20:34 +0400

    resized Appbar Logo Label

--- src/views/layout/Appbar.tsx
+++ src/views/layout/Appbar.tsx
@@ -11,8 +11,8 @@ const Appbar: React.FC<AppbarProps> = ({name}) => {
   return (
     <div className="w-screen h-16 border-b border-gray-100 shadow flex flex-row items-center justify-between px-4">
       <div className="h-full flex flex-row items-center">
-        <img className="h-full py-1" src={LogoDevice} alt="KeepRightDevice" />
-        <img className="-ml-2" src={LogoLabel} alt="KeepRightLabel" />
+        <img className="h-full py-1 z-10" src={LogoDevice} alt="KeepRightDevice" />
+        <img className="-ml-4 h-full py-3 z-0" src={LogoLabel} alt="KeepRightLabel" />
       </div>
       <button className="text-gray-600" onClick={logout}>
         Logout
