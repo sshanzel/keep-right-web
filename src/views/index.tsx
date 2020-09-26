9f5b1d457e1a799c5171d47aa499e135f7bd4206
Hansel Solevilla <sshanzel@yahoo.com>
2020-09-26 20:04:20 +0400

    sample changes

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,12 +3,14 @@ import Feed from './Feed';
 import KRInput from 'src/components/KRInput';
 import Something from 'src/entities/Something';
 import {getSomethingsByUserId} from 'src/__mocks__/Something';
+import KRModal from 'src/components/KRModal';
 
 export interface KeepRightProps {
   name: string;
 }
 
 const KeepRight: React.FC<KeepRightProps> = ({name}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
   const [somethings, setSomethings] = React.useState<Something[]>([]);
 
   React.useEffect(() => {
@@ -19,6 +21,27 @@ const KeepRight: React.FC<KeepRightProps> = ({name}) => {
     <div className="flex flex-1 flex-col overflow-auto">
       <div className="w-full bg-white mb-4 p-4 font-semibold">Hi {name}!</div>
       <KRInput shadow className="rounded-full px-4" />
+      <button className="bg-gray-500 m-4" onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+      <KRModal open={isOpen} onClose={() => setIsOpen(false)}>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+        <div>Sample</div>
+      </KRModal>
       <Feed somethings={somethings} />
     </div>
   );
